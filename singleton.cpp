0faed8b61a822d3222042be3485d7007ef03ff23
indm5 <indm504303@gmail.com>
2026-05-04 17:23:27 +0300

    Delete singleton.cpp

--- singleton.cpp
+++
@@ -1,9 +0,0 @@
-#include "singleton.h"
-
-SingletonDestroyer::~SingletonDestroyer()
-{
-    delete p_instance;
-}
-
-Singleton*         Singleton::p_instance = nullptr;
-SingletonDestroyer Singleton::destroyer;
